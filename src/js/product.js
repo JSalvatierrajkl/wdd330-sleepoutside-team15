e5eca3866e7373e8705b50604067e4d0096aea9f
JSalvatierrajkl <joeljsalvatierra@gmail.com>
2025-10-29 22:22:42 -0300

    W1 changes

--- src/js/product.js
+++ src/js/product.js
@@ -4,10 +4,9 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const existing = getLocalStorage('so-cart');
-  const cart = Array.isArray(existing) ? existing : existing ? [existing] : [];
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
